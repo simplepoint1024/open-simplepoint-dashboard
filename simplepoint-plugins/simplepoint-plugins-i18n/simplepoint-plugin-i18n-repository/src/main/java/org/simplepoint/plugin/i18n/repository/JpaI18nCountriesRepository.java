7deda6095e7df100909d675f4ca0ba264ade6f08
刘金旭 <somesimpled@gmail.com>
2025-10-31 16:04:58 +0800

    feat(client): Add i18n supports!

--- simplepoint-plugins/simplepoint-plugins-i18n/simplepoint-plugin-i18n-repository/src/main/java/org/simplepoint/plugin/i18n/repository/JpaI18nCountriesRepository.java
+++ simplepoint-plugins/simplepoint-plugins-i18n/simplepoint-plugin-i18n-repository/src/main/java/org/simplepoint/plugin/i18n/repository/JpaI18nCountriesRepository.java
@@ -2,7 +2,7 @@ package org.simplepoint.plugin.i18n.repository;
 
 import org.simplepoint.data.jpa.base.BaseRepository;
 import org.simplepoint.plugin.i18n.api.entity.Countries;
-import org.simplepoint.plugin.i18n.api.repository.CountriesRepository;
+import org.simplepoint.plugin.i18n.api.repository.I18nCountriesRepository;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -12,6 +12,6 @@ import org.springframework.stereotype.Repository;
  * This interface is used to interact with the persistence layer for Countries entities.
  */
 @Repository
-public interface JpaCountriesRepository extends BaseRepository<Countries, String>,
-    CountriesRepository {
+public interface JpaI18nCountriesRepository extends BaseRepository<Countries, String>,
+    I18nCountriesRepository {
 }
--- simplepoint-plugins/simplepoint-plugins-i18n/simplepoint-plugin-i18n-service/src/main/java/org/simplepoint/plugin/i18n/service/impl/I18nCountriesServiceImpl.java
+++ simplepoint-plugins/simplepoint-plugins-i18n/simplepoint-plugin-i18n-service/src/main/java/org/simplepoint/plugin/i18n/service/impl/I18nCountriesServiceImpl.java
@@ -2,8 +2,8 @@ package org.simplepoint.plugin.i18n.service.impl;
 
 import org.simplepoint.core.base.service.impl.BaseServiceImpl;
 import org.simplepoint.plugin.i18n.api.entity.Countries;
-import org.simplepoint.plugin.i18n.api.repository.CountriesRepository;
-import org.simplepoint.plugin.i18n.api.service.CountriesService;
+import org.simplepoint.plugin.i18n.api.repository.I18nCountriesRepository;
+import org.simplepoint.plugin.i18n.api.service.I18nCountriesService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -12,15 +12,15 @@ import org.springframework.stereotype.Service;
  * This service is used to interact with the persistence layer for country entities.
  */
 @Service
-public class CountriesServiceImpl extends BaseServiceImpl<CountriesRepository, Countries, String>
-    implements CountriesService {
+public class I18nCountriesServiceImpl extends BaseServiceImpl<I18nCountriesRepository, Countries, String>
+    implements I18nCountriesService {
 
   /**
    * Constructs a BaseServiceImpl with the specified repository and access metadata sync service.
    *
    * @param repository the repository to be used for entity operations
    */
-  public CountriesServiceImpl(CountriesRepository repository) {
+  public I18nCountriesServiceImpl(I18nCountriesRepository repository) {
     super(repository);
   }
 }
